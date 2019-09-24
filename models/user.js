d1a67d05546e299d45f816b818551844cfce7d0e
Krithika Subramanian <ksubram2@students.kennesaw.edu>
2019-09-24 10:49:43 -0400

    Backend done

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 //save a reference to the schema constructor
 
@@ -6,53 +7,71 @@ const Schema = mongoose.Schema;
 
 //using the schema constructor, create a new TodosSchema object
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     username: {
         type: String,
-        trim: true,
-        required: "Username is Required"
+        required: true,
+        unique: true
     },
     password: {
         type: String,
-        trim: true,
-        required: "Password is required",
+        required: true,
         validate: [
             function (input) {
                 return input.length >= 6;
             },
             "Password should be longer."
         ]
-    },
-    spotify: {
-        username: {
-            type: String,
-            trim: true,
-            required: true
-        },
-        password: {
-            type: String,
-            trim: true,
-            required: true
-        }
-    },
-    todos: { type: Array },
-    name: {
-        type: String,
-        trim: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-    },
-    genre: { type: String }
+    }
+    // spotify: {
+    //     username: {
+    //         type: String,
+    //         trim: true,
+    //         required: true
+    //     },
+    //     password: {
+    //         type: String,
+    //         trim: true,
+    //         required: true
+    //     }
+    // },
+    // todos: { type: Array },
+    // name: {
+    //     type: String,
+    //     trim: true
+    // },
+    // email: {
+    //     type: String,
+    //     unique: true,
+    //     match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    // },
+    // genre: { type: String }
 
 
 });
 
+UserSchema.methods.comparePassword = function (inputPass) {
+
+    return bcrypt.compareSync(inputPass, this.password);
+};
+
+
+
+UserSchema.pre("save", function (next) {
+
+    //this.password - "actual password"
+
+    //Rehash onlyif they update password
+
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hashSync(this.password, 10)
+    return next();
+});
+
 //This creates our model from the above schema, using mongoose model method
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 
 //Export the Todos model
 
