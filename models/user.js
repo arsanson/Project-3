e16db7b748b1094ed817ffd894bfca591844740a
Krithika Subramanian <ksubram2@students.kennesaw.edu>
2019-09-26 13:05:00 -0400

    Spotify added

--- models/user.js
+++ models/user.js
@@ -8,40 +8,36 @@ const Schema = mongoose.Schema;
 //using the schema constructor, create a new TodosSchema object
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        validate: [
-            function (input) {
-                return input.length >= 6;
-            },
-            "Password should be longer."
-        ]
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: [
+      function(input) {
+        return input.length >= 6;
+      },
+      "Password should be longer."
+    ]
+  }
 });
 
-UserSchema.methods.comparePassword = function (inputPass) {
-
-    return bcrypt.compareSync(inputPass, this.password);
+UserSchema.methods.comparePassword = function(inputPass) {
+  return bcrypt.compareSync(inputPass, this.password);
 };
 
+UserSchema.pre("save", function(next) {
+  //this.password - "actual password"
 
+  //Rehash onlyif they update password
 
-UserSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
 
-    //this.password - "actual password"
-
-    //Rehash onlyif they update password
-
-    if (!this.isModified("password")) return next();
-
-    this.password = bcrypt.hashSync(this.password, 10)
-    return next();
+  this.password = bcrypt.hashSync(this.password, 10);
+  return next();
 });
 
 //This creates our model from the above schema, using mongoose model method
