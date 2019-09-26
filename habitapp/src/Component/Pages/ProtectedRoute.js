ce94f548d73bcadeeb78097d3fc88fc8c97639ea
Balwinder Singh <singhbal95@yahoo.comgit config user.emailgit config -- global user.email singhbal95@yahoo.com>
2019-09-26 11:42:38 -0400

    inputbox added

--- habitapp/src/Component/Pages/ProtectedRoute.js
+++ habitapp/src/Component/Pages/ProtectedRoute.js
@@ -3,11 +3,13 @@ import { Route, Redirect } from "react-router-dom";
 import Auth from "../../utils/Auth";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-        Auth.isLoggedIn()
-            ? <Component {...props} />
-            : <Redirect to="/login" />
-    )} {...rest} />
+  <Route
+    {...rest}
+    render={props =>
+      Auth.isLoggedIn() ? <Component {...props} /> : <Redirect to="/login" />
+    }
+    {...rest}
+  />
 );
 
 export default ProtectedRoute;
