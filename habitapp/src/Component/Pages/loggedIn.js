087731a5a934ecaf9508d36be26fc96dc4b8c2df
Ravi Mantur <RaviMantur@RavisMac.local>
2019-09-26 13:45:08 -0400

    logout working fine

--- habitapp/src/Component/Pages/loggedIn.js
+++ habitapp/src/Component/Pages/loggedIn.js
@@ -3,8 +3,9 @@ import { Layout, Menu, Breadcrumb, Icon } from "antd";
 import "antd/dist/antd.css";
 import { Calendar } from "antd";
 import { List, Input } from "antd";
+import Auth from '../../utils/Auth'
 
-export default () => {
+export default props => {
 
 
 
@@ -30,6 +31,12 @@ export default () => {
         console.log(value, mode);
     }
 
+    const logOutHandler = () => {
+        Auth.logOut(() => {
+            props.history.push("/");
+        })
+    }
+
 
     return (
 
@@ -42,7 +49,7 @@ export default () => {
                     defaultSelectedKeys={['2']}
                     style={{ lineHeight: '64px' }}
                 >
-                    <Menu.Item key="1" >Logout</Menu.Item>
+                    <Menu.Item key="1" onClick={logOutHandler}>Logout</Menu.Item>
                     <Menu.Item key="2">Welcome!</Menu.Item>
                     <Menu.Item key='3' style={{ position: 'absolute', left: '50%' }}>
                         <Icon type="play-square" />
